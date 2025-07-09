399fee64f40425b3561b18e945ea6c91c941271c
Tirth <tirthacodes@gmail.com>
2025-07-09 23:48:58 +0545

    creating controller without template, and using template

---
+++ dot-net-core-web-api/Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dot_net_core_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudentName()
+        {
+            return "Student Name: Ronaldo suii";
+        }
+    }
+}
