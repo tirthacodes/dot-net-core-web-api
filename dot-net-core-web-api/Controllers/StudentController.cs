11eb02c3f4a0043d9ad5f9084af6e6902c437905
Tirth <tirthacodes@gmail.com>
2025-07-10 08:27:12 +0545

    in memory HttpDelete Test returning string as response

--- dot-net-core-web-api/Controllers/StudentController.cs
+++ dot-net-core-web-api/Controllers/StudentController.cs
@@ -18,5 +18,23 @@ namespace dot_net_core_web_api.Controllers
         {
             return CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
         }
+
+        [HttpGet("{name}")]
+        public Student GetStudentbyName(string name)
+        {
+            return CollegeRepository.Students.Where(n => n.StudentName == name).FirstOrDefault();
+        }
+
+        [HttpDelete("{id:int}", Name ="DeleteStudentbyID")]
+        public string DeleteStudentbyID(int id)
+        {
+            var student = GetStudentbyId(id);
+            if (student != null)
+            {
+                CollegeRepository.Students.Remove(student);
+                return $"Student with {id} successfully deleted!";
+            }
+            return "Student ID didn't match";
+        }
     }
 }
