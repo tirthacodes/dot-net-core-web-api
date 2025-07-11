d2adbda75b8865de96dbc7e19ada5903e093c05f
Tirth <tirthacodes@gmail.com>
2025-07-11 20:54:29 +0545

    required and email address validation in dto

--- dot-net-core-web-api/Model/StudentDTO.cs
+++ dot-net-core-web-api/Model/StudentDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace dot_net_core_web_api.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dot_net_core_web_api.Model
 {
     public class StudentDTO
     {
         public int Id { get; set; }
+        [Required]
         public string StudentName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Address { get; set; }
     }
 }
