1d496034a1e667b05fc2f87079a08b9d28f2e977
Tirth <tirthacodes@gmail.com>
2025-07-13 21:25:37 +0545

    Included Error messages in DTO

--- dot-net-core-web-api/Model/StudentDTO.cs
+++ dot-net-core-web-api/Model/StudentDTO.cs
@@ -1,13 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace dot_net_core_web_api.Model
 {
     public class StudentDTO
     {
+        [ValidateNever]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Student name is required.")]
         public string StudentName { get; set; }
-        [EmailAddress]
+        [EmailAddress(ErrorMessage ="Please enter valid email address.")]
         public string Email { get; set; }
         [Required]
         public string Address { get; set; }
