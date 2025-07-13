b56177196f74d8a48d01c1a2893dd0f828c8857d
Tirth <tirthacodes@gmail.com>
2025-07-13 21:46:13 +0545

    [Compare(nameof(Password))]

--- dot-net-core-web-api/Model/StudentDTO.cs
+++ dot-net-core-web-api/Model/StudentDTO.cs
@@ -7,11 +7,18 @@ namespace dot_net_core_web_api.Model
     {
         [ValidateNever]
         public int Id { get; set; }
+        [StringLength(30)] 
         [Required(ErrorMessage ="Student name is required.")]
         public string StudentName { get; set; }
         [EmailAddress(ErrorMessage ="Please enter valid email address.")]
         public string Email { get; set; }
+
+        [Range(10,20)]
+        public int Age {  get; set; }
         [Required]
         public string Address { get; set; }
+        public string Password { get; set; }
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
     }
 }
